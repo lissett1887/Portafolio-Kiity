3b8219b9b223aa4210237700ff06e74d7c89a06e
lissett1887 <162449609+lissett1887@users.noreply.github.com>
2024-10-23 12:16:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('contact-form').addEventListener('submit',
+function(event) {
+    event.preventDefault();
+
+    alert('Mensaje enviado con exito');
+
+    this.onsubmit();
+});
+
+
+function bienvenida(){
+    alert ("Bienvenidos!")
+}
+
+
